4f73dfc5a65477c3157a943819d53ad97714f676
김요셉 <joseph.kim@musinsa.com>
2020-07-22 12:28:42 +0900

    test

--- server.js
+++ server.js
@@ -13,7 +13,45 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-app.get('/get', (req, res) => {
+app.get('/test', (req, res) => {
+  const code = req.param('code');
+  const htmls = getHtml(code);
+
+  htmls.then(html => {
+    let ulList = [];
+    const $ = cheerio.load(html.data);
+    const current_price = $("#chart_area").children("div.rate_info").children("div.today").children("p.no_today").children("em");
+    const yesterday_per_price = $("#chart_area").children("div.rate_info").children("div.today").children("p.no_exday").children("em");
+    
+    const curr_price = current_price.children("span");
+    const upDown = yesterday_per_price.children("span").eq(0);
+    let isPlusMinus = "";
+    let isUpDown = "";
+    if( upDown.hasClass("up") ){
+      isUpDown = " 상승";
+      isPlusMinus = "+";
+    }else if( upDown.hasClass("down") ){
+      isUpDown = " 하락";
+      isPlusMinus = "-";
+    }
+    const unit = "원"
+    const per_won = yesterday_per_price.find("span.blind").html();
+
+    const won = "현재가 " + curr_price.html()+unit;
+    const letter = "전일대비 "+ per_won+ unit + isUpDown;
+    const percentage = isPlusMinus + yesterday_per_price.next().next().find("span.blind").html()+"%";
+
+    const result = {
+      "123"  : won,
+      "1" : percentage,
+      "2" : letter
+    };
+    
+    res.send(result);
+  });
+});
+
+app.post('/get', (req, res) => {
   const code = req.param('code');
   const htmls = getHtml(code);
   
